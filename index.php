146a260b7078975df9e3e6263f9ff7ffc2d1274f
Arinerron <arinesau@gmail.com>
2018-01-14 13:59:47 -0800

    add stuff

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
-    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-    if ($socket === false) {
-        echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-    } else {
-        $result = socket_connect($socket, '127.0.0.1', 3333);
-        if ($result === false) {
-            echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+    function sendMessage($in) {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            return false;
         } else {
-            $in = '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}';
-            socket_write($socket, $in, strlen($in));
-            while ($out = socket_read($socket, 2048)) {
-                echo $out;
+            $result = socket_connect($socket, '127.0.0.1', 3333);
+            if ($result === false) {
+                return false;
+            } else {
+                socket_write($socket, $in, strlen($in));
+
+                $out = '';
+                while ($outmsg = socket_read($socket, 2048)) {
+                    $out .= $outmsg;
+                }
+                socket_close($socket);
+
+                return $out;
             }
-            socket_close($socket);
         }
     }
 ?>
@@ -22,5 +27,6 @@
         <title>Mining Monitor</title>
     </head>
     <body>
+        <h1><?php echo sendMessage('{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}');?></h1>
     </body>
 </html>
